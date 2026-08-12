d285da0508a6e64b2f230321e595ad192290f465
Gavin Wilson <gmanwilson@icloud.com>
2026-08-12 14:55:49 -0400

    edit: allow credentials

--- backend/NexusAPI/Program.cs
+++ backend/NexusAPI/Program.cs
@@ -27,7 +27,8 @@ builder.Services.AddCors(options =>
         policy
             .WithOrigins("http://localhost:3000")
             .AllowAnyHeader()
-            .AllowAnyMethod();
+            .AllowAnyMethod()
+            .AllowCredentials();
     });
 });
 
